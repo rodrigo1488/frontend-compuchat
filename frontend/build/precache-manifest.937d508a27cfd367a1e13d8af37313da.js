f3bb76aa815a2c17b037a23b8a7a3962e4f44abc
Rodrigo 1488 <gomesrodrigo528@gmail.com>
2025-11-21 15:56:47 -0300

    primeiro push dessa porra

---
+++ frontend/build/precache-manifest.937d508a27cfd367a1e13d8af37313da.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd6d0eb0f0ca49545e154340595e6394",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc67afd192d70ac012c7",
+    "url": "/static/css/2.cd8a6809.chunk.css"
+  },
+  {
+    "revision": "734033b8da05521003c3",
+    "url": "/static/css/main.4ab74a11.chunk.css"
+  },
+  {
+    "revision": "fc67afd192d70ac012c7",
+    "url": "/static/js/2.79d31fd3.chunk.js"
+  },
+  {
+    "revision": "4ca9e05620d0bead510abbf71a1260a3",
+    "url": "/static/js/2.79d31fd3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a3402aaf05f8c319a6d",
+    "url": "/static/js/3.89ac163b.chunk.js"
+  },
+  {
+    "revision": "d98f98f9156b1385986c1cdc16f69da5",
+    "url": "/static/js/3.89ac163b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "734033b8da05521003c3",
+    "url": "/static/js/main.41277bcc.chunk.js"
+  },
+  {
+    "revision": "e42399a1400f274c1028",
+    "url": "/static/js/runtime-main.4768d814.js"
+  },
+  {
+    "revision": "2529c2597bdd5b6559aa1e276d74e930",
+    "url": "/static/media/chat_notify.2529c259.mp3"
+  },
+  {
+    "revision": "547b24bd7d4cc6a58cf3864d650fb152",
+    "url": "/static/media/logo.547b24bd.png"
+  },
+  {
+    "revision": "5c788b8d54d3ebf021114528116dfd77",
+    "url": "/static/media/n8n.5c788b8d.png"
+  },
+  {
+    "revision": "549edebaf5bafe89b539e694d4b1e26c",
+    "url": "/static/media/planilha.549edeba.xlsx"
+  },
+  {
+    "revision": "3b8df420d8c163a8574d63940d258de0",
+    "url": "/static/media/sound.3b8df420.mp3"
+  },
+  {
+    "revision": "8244f0423cb09d847ac99e7546cb9220",
+    "url": "/static/media/wa-background-dark.8244f042.png"
+  },
+  {
+    "revision": "e55b8eea411acd5062612f32a8aef153",
+    "url": "/static/media/wa-background.e55b8eea.png"
+  },
+  {
+    "revision": "e5374d73ff43538fdaa16a3f1a980f4b",
+    "url": "/static/media/webhook.e5374d73.png"
+  }
+]);
